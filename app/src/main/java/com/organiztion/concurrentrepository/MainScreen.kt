9d13e89729aae28f9bc8a3b78056bc451d09c71c
Ivan Mikhailov <imihailov@findmykids.org>
2025-06-21 11:24:21 +0300

    Add example concurrent work

---
+++ app/src/main/java/com/organiztion/concurrentrepository/MainScreen.kt
@@ -0,0 +1,55 @@
+package com.organiztion.concurrentrepository
+
+import com.organiztion.concurrentrepository.repository.DataApi
+import com.organiztion.concurrentrepository.repository.DataRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class Feature1(private val dataRepository: DataRepository) {
+    val scope = CoroutineScope(Dispatchers.IO)
+    fun execute() {
+        scope.launch {
+            dataRepository.update()
+            //Do some work
+            println("Feature1 work with ${dataRepository.getData()}")
+        }
+    }
+}
+
+class Feature2(private val dataRepository: DataRepository) {
+    val scope = CoroutineScope(Dispatchers.IO)
+    fun execute() {
+        scope.launch {
+            dataRepository.update()
+            //Do some work
+            println("Feature2 work with ${dataRepository.getData()}")
+        }
+    }
+}
+
+class Feature3(private val dataRepository: DataRepository) {
+    val scope = CoroutineScope(Dispatchers.IO)
+    fun execute() {
+        scope.launch {
+            dataRepository.update()
+            //Do some work
+            println("Feature3 work with ${dataRepository.getData()}")
+        }
+    }
+}
+
+fun main() {
+    val repository = DataRepository(DataApi())
+    val feature1 = Feature1(repository)
+    val feature2 = Feature2(repository)
+    val feature3 = Feature3(repository)
+
+    while(true) {
+        println("Doing some independent work")
+        feature1.execute()
+        feature2.execute()
+        feature3.execute()
+        Thread.sleep(5000)
+    }
+}
---
+++ app/src/main/java/com/organiztion/concurrentrepository/repository/DataApi.kt
@@ -0,0 +1,34 @@
+package com.organiztion.concurrentrepository.repository
+
+data class ObjectDto(val id: Int, val name: String, val age: Int)
+
+class DataApi {
+    fun getData(): List<ObjectDto> {
+        println("Start getData api")
+        val list = listOf(
+            ObjectDto(1, randomName(), randomNumber()),
+            ObjectDto(2, randomName(), randomNumber()),
+            ObjectDto(3, randomName(), randomNumber()),
+            ObjectDto(4, randomName(), randomNumber()),
+            ObjectDto(5, randomName(), randomNumber()),
+            ObjectDto(6, randomName(), randomNumber()),
+            ObjectDto(7, randomName(), randomNumber()),
+            ObjectDto(8, randomName(), randomNumber()),
+            ObjectDto(9, randomName(), randomNumber()),
+        )
+        Thread.sleep((5..10).random() * 1000L)
+        println("End getData api")
+        return list
+    }
+}
+
+fun randomNumber() = (1..20).random()
+fun randomName() = listOf(
+    "Ivan",
+    "Kirill",
+    "Oleg",
+    "Dmitry",
+    "Mikhail",
+    "Aleksandr",
+    "Ilya"
+).random()
